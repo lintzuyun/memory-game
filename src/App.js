8404742073940e35d47f58361a745425cf597029
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-12-03 15:15:07 +0000

    add function to shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "../public/img/1.png" },
+  { src: "../public/img/2.png" },
+  { src: "../public/img/3.png" },
+  { src: "../public/img/4.png" },
+  { src: "../public/img/5.png" },
+  { src: "../public/img/6.png" },
+];
+
 function App() {
-  return <div>hey</div>;
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //shuffle cards
+  function shuffleCards() {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0);
+  }
+
+  console.log(cards, turns);
+
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
 }
 
 export default App;
